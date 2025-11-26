f951ed6297891cd2f4ee728f2eac4bd776271d5a
JoseV-001 <Leaphaz@gmail.com>
2025-11-25 21:11:19 -0300

    Versão 4 - nova implementação da interface

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,6 +5,7 @@ import model.entities.Product;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,7 +17,11 @@ public class Main {
         list.add(new Product("Tablet", 350.00));
         list.add(new Product("HD Case", 80.90));
 
-        list.removeIf(Product::nonStaticProductPredicate);
+        double min = 100.0; // Posso obter ele sendo digitado pelo User
+
+        Predicate<Product> pred = p -> p.getPrice() >= min;
+
+        list.removeIf(pred);
 
         for (Product p : list){
             System.out.println(p);
